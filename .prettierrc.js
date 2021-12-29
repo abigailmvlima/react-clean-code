799bec78283d556b35b9d074651ad42d06f3aff2
elio.lima <elio.designer@hotmail.com>
2021-12-29 02:21:07 -0300

    create store

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    singleQuote: true,
+    trailingComma: 'all',
+    printWidth: 100,
+    arrowParens: 'always',
+    jsxBracketSameLine: true,
+    bracketSpacing: true,
+    semi: false,
+    tabWidth: 2,
+    useTabs: true
+}
