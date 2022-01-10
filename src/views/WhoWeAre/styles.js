9c09018379a577b115e425b6de2ef527579582f2
Abigail Lima <abigailmvlima@outlook.com>
2022-01-09 22:49:55 -0300

    add who we are

---
+++ src/views/WhoWeAre/styles.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+	display: flex;
+`
+
+export const ColDescripitions = styled.div`
+	display: flex;
+	width: 50%;
+	flex-direction: column;
+	background-color: #fff;
+`
+
+export const ColImg = styled.div`
+	display: flex;
+	width: 50%;
+`
+
+export const Descripition = styled.div`
+	display: flex;
+	font-size: 20px;
+	padding: 10px 20px;
+	text-align: justify;
+`
